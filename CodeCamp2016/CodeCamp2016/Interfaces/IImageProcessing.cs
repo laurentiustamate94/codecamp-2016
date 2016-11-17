e67e19c80cbe362fddf6a9213e63bf304d3d0085
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2016-11-17 18:07:06 +0200

    Image Processing - add interface to specify an abstract way to handle image processing

---
+++ CodeCamp2016/CodeCamp2016/Interfaces/IImageProcessing.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace CodeCamp2016.Interfaces
+{
+    public interface IImageProcessing
+    {
+        Task<object> AnalyzeImage(string imagePath);
+
+        Task<string> GetDominantForegroundColor(string imagePath);
+
+        Task<string> RecognizeText(string imagePath);
+
+        Task<bool> RecognizeFace(string imagePath);
+
+        Task<string> RecognizeEmotion(string imagePath);
+    }
+}
