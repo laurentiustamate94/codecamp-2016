1f535d96dbb650e4f269ac731ee7f171a5e588bd
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2016-11-17 18:06:31 +0200

    Image Source - specify an abstract way to handle image sources

---
+++ CodeCamp2016/CodeCamp2016/ImageSource/LocalCameraImageSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using CodeCamp2016.Interfaces;
+using Windows.Media.Capture;
+
+namespace CodeCamp2016.ImageSource
+{
+    public class LocalCameraImageSource : IImageSource
+    {
+        public IPhotoStorage Storage { get; set; }
+
+        public MediaCapture MediaDevice { get; private set; }
+
+        public LocalCameraImageSource(IPhotoStorage storage, MediaCapture mediaDevice)
+        {
+            Storage = storage;
+            MediaDevice = mediaDevice;
+        }
+
+        public async Task InitializeDevice()
+        {
+            await MediaDevice.InitializeAsync();
+
+            MediaDevice.Failed += new MediaCaptureFailedEventHandler(MediaCapture_Failed);
+        }
+
+        public void TakePhoto(string filename)
+        {
+            Storage.Save(filename);
+        }
+
+        private async void MediaCapture_Failed(MediaCapture currentCaptureObject, MediaCaptureFailedEventArgs currentFailure)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeCamp2016/CodeCamp2016/Interfaces/IImageSource.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace CodeCamp2016.Interfaces
+{
+    public interface IImageSource
+    {
+        IPhotoStorage Storage { get; set; }
+
+        Task InitializeDevice();
+
+        void TakePhoto(string filename);
+    }
+}
