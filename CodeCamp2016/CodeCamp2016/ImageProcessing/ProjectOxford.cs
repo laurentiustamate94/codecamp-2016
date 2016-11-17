8353d828fac6589388e5473574690597cc389c74
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2016-11-17 18:07:31 +0200

    Add base partial Image Processing class

---
+++ CodeCamp2016/CodeCamp2016/ImageProcessing/ProjectOxford.cs
@@ -0,0 +1,24 @@
+﻿using CodeCamp2016.Interfaces;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Vision;
+using Windows.ApplicationModel.Resources;
+
+namespace CodeCamp2016.ImageProcessing
+{
+    public partial class ProjectOxford : IImageProcessing
+    {
+        private readonly IVisionServiceClient visionClient;
+        private readonly IFaceServiceClient faceClient;
+        private readonly IEmotionServiceClient emotionClient;
+
+        public ProjectOxford()
+        {
+            var appSettings = ResourceLoader.GetForViewIndependentUse("AppSettings");
+
+            visionClient = new VisionServiceClient(appSettings.GetString("VISION_API_KEY"));
+            faceClient = new FaceServiceClient(appSettings.GetString("FACE_API_KEY"));
+            emotionClient = new EmotionServiceClient(appSettings.GetString("EMOTION_API_KEY"));
+        }
+    }
+}
